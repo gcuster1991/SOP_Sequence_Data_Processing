5c47fa0dc990d5ec1b800fa10460846dcfdba754
Gordon Custer <gcuster@uwyo.edu>
2019-12-10 08:35:08 -0700

    Add files via upload

---
+++ merge.sh
@@ -0,0 +1,35 @@
+!#bin/bash
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P11A*R1*fastq -reverse P11A*R2*fastq -fastqout merged/P11A_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P11B*R1*fastq -reverse P11B*R2*fastq -fastqout merged/P11B_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P21A*R1*fastq -reverse P21A*R2*fastq -fastqout merged/P21A_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P21B*R1*fastq -reverse P21B*R2*fastq -fastqout merged/P21B_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P31A*R1*fastq -reverse P31A*R2*fastq -fastqout merged/P31A_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P31B*R1*fastq -reverse P31B*R2*fastq -fastqout merged/P31B_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P41A*R1*fastq -reverse P41A*R2*fastq -fastqout merged/P41A_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P51A*R1*fastq -reverse P51A*R2*fastq -fastqout merged/P51A_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P51B*R1*fastq -reverse P51B*R2*fastq -fastqout merged/P51B_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P61A*R1*fastq -reverse P61A*R2*fastq -fastqout merged/P61A_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P61B*R1*fastq -reverse P61B*R2*fastq -fastqout merged/P61B_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P71A*R1*fastq -reverse P71A*R2*fastq -fastqout merged/P71A_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P71B*R1*fastq -reverse P71B*R2*fastq -fastqout merged/P71B_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P81B*R1*fastq -reverse P81B*R2*fastq -fastqout merged/P81B_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+//Users/gordoncuster/Desktop/usearch11 -fastq_mergepairs P91B*R1*fastq -reverse P91B*R2*fastq -fastqout merged/P91B_merged.fastq -fastq_maxdiffs 10 -fastq_pctid 10 -fastq_minmergelen 250 -fastq_maxmergelen 253 -report merged_report.txt
+
+
+#script written by Gordon Custer.
+#Uses Usearch which must be installed at location given in script. 
